b6f92c382910dc8bd471e7e09a4f67858c7377bd
vayneluo <ryu_cn@163.com>
2019-10-11 16:34:40 +0800

    spring boot 集成各种骚操作

---
+++ boot-integrate/src/main/java/com/xiaoluo/boot/integrate/BootIntegrateApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoluo.boot.integrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootIntegrateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootIntegrateApplication.class, args);
+    }
+
+}
