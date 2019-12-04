838b384e12e3c9fd5e62d9fc3ee44360799a6d26
vayneluo <ryu_cn@163.com>
2019-12-04 11:04:57 +0800

    测试代码合并

--- java-design/src/main/java/com/xiaoluo/java/design/threadpool/ThreadLocalDemo.java
+++ java-design/src/main/java/com/xiaoluo/java/design/threadpool/ThreadLocalDemo.java
@@ -9,6 +9,7 @@ public class ThreadLocalDemo {
     int threads = 3;
     CountDownLatch countDownLatch = new CountDownLatch(threads);
     InnerClass innerClass = new InnerClass();
+    System.out.println("1.0.1分支修改了此处代码");
     for(int i = 1; i <= threads; i++) {
       new Thread(() -> {
         for(int j = 0; j < 4; j++) {
@@ -17,6 +18,7 @@ public class ThreadLocalDemo {
         }
         innerClass.set("hello world");
         countDownLatch.countDown();
+        System.out.println("1.0.1分支修改了此处代码");
       }, "thread - " + i).start();
     }
     countDownLatch.await();
